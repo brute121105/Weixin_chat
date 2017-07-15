b9cb2d9de842750cba74983ebc7c430ddae0a8cc
from ebtong <752905288@qq.com>
2017-07-15 11:30:18 +0800

    6.5.10 实践2017年7月15日11:30:10

--- app/src/main/java/hyj/weinxin_chat/ChatService.java
+++ app/src/main/java/hyj/weinxin_chat/ChatService.java
@@ -1,13 +1,13 @@
 package hyj.weinxin_chat;
 
 import android.accessibilityservice.AccessibilityService;
-import android.content.ClipData;
-import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.os.Looper;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.Toast;
 
 
 import java.text.SimpleDateFormat;
@@ -80,7 +80,7 @@ public class ChatService extends AccessibilityService {
                 LogUtil.d("autoChat","等待自动登录完成...");
                 return;
             }
-            if(accounts.size()>0&&minute%intevalLoginTime==0&&Math.abs(minute-lastLoginMinute)>1){
+            if(intevalLoginTime!=0&&accounts.size()>0&&minute%intevalLoginTime==0&&Math.abs(minute-lastLoginMinute)>1){
                 synchronized (this){
                     LogUtil.d("autoLogin","满足自动登录");;
                     AccessibilityNodeInfo loginRoot = getRootInActiveWindow();
@@ -101,18 +101,41 @@ public class ChatService extends AccessibilityService {
                 }
             }
 
+            AccessibilityNodeInfo ssBtn = AutoUtil.findNodeInfosById(getRootInActiveWindow(),"com.tencent.mm:id/g8");
+            if(ssBtn!=null){
+                AccessibilityNodeInfo ss =  ssBtn.getChild(1);
+                AutoUtil.performClick(ss,record,"搜索");
+                //输入微信id
+                findNodeAndSetText(1);
+                AutoUtil.sleep(500);
+                //点击搜索结果
+                findNodeAndClickId(1,"com.tencent.mm:id/j9");
+                //判断是否有键盘按钮
+                findNodeAndClickId(1,"com.tencent.mm:id/a5j");
+                //填充文本
+                findEditAndSetText(1);
+                //点击发送
+                findNodeAndClickId(1,"com.tencent.mm:id/a5k");
+                AutoUtil.sleep(300);
+                //返回
+                findNodeAndClickId(1,"com.tencent.mm:id/gr");
+                //back2List(getRootInActiveWindow());
+                //back2List(getRootInActiveWindow());
+            }
+
+            if(true) return;
             //while (true){
                 AccessibilityNodeInfo root = getRootInActiveWindow();
                 if(root!=null){
                     if(nickNames.isEmpty()){
                         getNickNamesList(root);
                     }
-                    List<AccessibilityNodeInfo> listView= root.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/bny");
+                    List<AccessibilityNodeInfo> listView= root.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/bpl");
                     if(listView!=null&&listView.size()>0) {
                         for (int i = 0, l = listView.get(0).getChildCount(); i < l; i++) {
                             String  nickText = "";
                             AccessibilityNodeInfo childNode = listView.get(0).getChild(i);
-                            AccessibilityNodeInfo nickNode = AutoUtil.findNodeInfosById(childNode, "com.tencent.mm:id/agw");
+                            AccessibilityNodeInfo nickNode = AutoUtil.findNodeInfosById(childNode, "com.tencent.mm:id/aig");
                             if (nickNode != null) {
                                 nickText = nickNode.getText().toString();
                                 System.out.println("nick-->" + nickText);
@@ -123,7 +146,7 @@ public class ChatService extends AccessibilityService {
                                 //6、填充第3步已获取消息到输入框
                                 findEditAndSetText(0);
                                 if(!AutoUtil.checkAction(record,Constants.CHAT_ACTION_05)){
-                                    AccessibilityNodeInfo keyBtn = AutoUtil.findNodeInfosById(getRootInActiveWindow(),"com.tencent.mm:id/a47");
+                                    AccessibilityNodeInfo keyBtn = AutoUtil.findNodeInfosById(getRootInActiveWindow(),"com.tencent.mm:id/a5c");
                                     if(keyBtn!=null) {
                                         LogUtil.d("autoChat","键盘按钮已获取");
                                         AutoUtil.performClick(keyBtn, record, "点击键盘按钮",500);
@@ -132,11 +155,6 @@ public class ChatService extends AccessibilityService {
                                         LogUtil.d("autoChat","键盘按钮 is null");
                                     }
                                 }
-                               /* AccessibilityNodeInfo editText = AutoUtil.findNodeInfosById(getRootInActiveWindow(),"com.tencent.mm:id/a49");
-                                if(editText!=null){
-                                    editText.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT,AutoUtil.createBuddleText("测试内容"+System.currentTimeMillis()+" "+countSendNum));
-                                    AutoUtil.recordAndLog(record,Constants.CHAT_ACTION_05);
-                                }*/
                                 //7、发送
                                 if(Constants.CHAT_ACTION_05.equals(record.get("recordAction"))){
                                     root = getRootInActiveWindow();
@@ -160,9 +178,63 @@ public class ChatService extends AccessibilityService {
             //}
         }
     }
+    private List<String> getWxids(){
+        List<String> ids = new ArrayList<String>();
+        ids.add("bh4073");
+        ids.add("mm77375");
+        ids.add("mt74196");
+        ids.add("ok75581");
+        ids.add("bbk4872");
+        ids.add("cba4871");
+        ids.add("cba4870");
+        ids.add("nnk4873");
+        ids.add("ss978692");
+        ids.add("zhq686366");
+        ids.add("hyj5690");
+        return ids;
+    }
+    static int wxIdIndex=0;
+    private void findNodeAndSetText(int tryCount){
+        if(tryCount==10) return;
+        AccessibilityNodeInfo ssEdit =  AutoUtil.findNodeInfosById(getRootInActiveWindow(),"com.tencent.mm:id/h2");
+        if(ssEdit!=null){
+            AutoUtil.performSetText(ssEdit,getWxids().get(wxIdIndex),record,"搜索微信id");
+            if(wxIdIndex==getWxids().size()-1){
+                wxIdIndex = 0;
+            }else {
+                wxIdIndex = wxIdIndex+1;
+            }
+        }else if(tryCount!=0){
+            LogUtil.d("autoChat","搜索框 is null "+tryCount);
+            AutoUtil.sleep(200);
+            findNodeAndSetText(tryCount+1);
+        }
+    }
+    private void findNodeAndClickId(int tryCount,String id){
+        if(tryCount==10) return;
+        AccessibilityNodeInfo node= AutoUtil.findNodeInfosById(getRootInActiveWindow(),id);;
+        if(node!=null){
+            AutoUtil.performClick(node,record,"点击"+id);
+        }else if(tryCount!=0){
+            LogUtil.d("autoChat",id+" is null "+tryCount);
+            AutoUtil.sleep(200);
+            findNodeAndClickId(tryCount+1,id);
+        }
+    }
+    private void findNodeAndClickText(int tryCount,String text){
+        if(tryCount==10) return;
+        AccessibilityNodeInfo node= AutoUtil.findNodeInfosById(getRootInActiveWindow(),text);;
+        if(node!=null){
+            AutoUtil.performClick(node,record,"点击"+text);
+        }else if(tryCount!=0){
+            LogUtil.d("autoChat",text+" is null "+tryCount);
+            AutoUtil.sleep(200);
+            findNodeAndClickText(tryCount+1,text);
+        }
+    }
     private void findEditAndSetText(int tryCount){
         if(tryCount==10) return;
-        AccessibilityNodeInfo editText = AutoUtil.findNodeInfosById(getRootInActiveWindow(),"com.tencent.mm:id/a49");
+        AccessibilityNodeInfo editText = AutoUtil.findNodeInfosById(getRootInActiveWindow(),"com.tencent.mm:id/a5e");
         if(editText!=null){
             List<String> msgs = AutoUtil.getSomeMsgs();
             int randomNum = (int)(Math.random()*msgs.size());
@@ -170,22 +242,22 @@ public class ChatService extends AccessibilityService {
             AutoUtil.recordAndLog(record,Constants.CHAT_ACTION_05);
         }else if(tryCount!=0) {
             LogUtil.d("autoChat","输入框 is null "+tryCount);
-            AutoUtil.sleep(500);
+            AutoUtil.sleep(200);
             findEditAndSetText(tryCount+1);
         }
     }
     private void back2List(AccessibilityNodeInfo root){
-        AccessibilityNodeInfo backBtn = AutoUtil.findNodeInfosById(root,"com.tencent.mm:id/go");
+        AccessibilityNodeInfo backBtn = AutoUtil.findNodeInfosById(root,"com.tencent.mm:id/gr");
         AutoUtil.performClick(backBtn,record,"全局返回");
     }
     private void getNickNamesList(AccessibilityNodeInfo root){
         if(root!=null) {
-            List<AccessibilityNodeInfo> listView = root.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/bny");
+            List<AccessibilityNodeInfo> listView = root.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/bpl");
             if (listView != null && listView.size() > 0) {
                 for (int i = 0, l = listView.get(0).getChildCount(); i < l; i++) {
                     String nickText = "";
                     AccessibilityNodeInfo childNode = listView.get(0).getChild(i);
-                    AccessibilityNodeInfo nickNode = AutoUtil.findNodeInfosById(childNode, "com.tencent.mm:id/agw");
+                    AccessibilityNodeInfo nickNode = AutoUtil.findNodeInfosById(childNode, "com.tencent.mm:id/aig");
                     if (nickNode != null) {
                         nickText = nickNode.getText().toString();
                         System.out.println("getNickNamesList nick-->" + nickText);
@@ -220,13 +292,14 @@ public class ChatService extends AccessibilityService {
         //1、退出当前账号
         if(AutoUtil.checkAction(loginRecord,Constants.LOGIN_ACTION_00)){
             AccessibilityNodeInfo exitCurrentAcountBtn = AutoUtil.findNodeInfosByText(nodeInfo,"退出当前帐号");
+            System.out.println("exitCurrentAcountBtn--->"+exitCurrentAcountBtn);
             AutoUtil.performClick(exitCurrentAcountBtn,loginRecord,Constants.LOGIN_ACTION_01);
             return;
         }
 
         //2、确认退出
         if(AutoUtil.checkAction(loginRecord,Constants.LOGIN_ACTION_01)){
-            AccessibilityNodeInfo quiteConfirmBtn = nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/bu8").get(0).getChild(1);
+            AccessibilityNodeInfo quiteConfirmBtn = nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/bvw").get(0).getChild(1);
             AutoUtil.performClick(quiteConfirmBtn,loginRecord,Constants.LOGIN_ACTION_02);
             return;
         }
@@ -238,31 +311,29 @@ public class ChatService extends AccessibilityService {
         }
         //4、切换账号
         if(AutoUtil.checkAction(loginRecord,Constants.LOGIN_ACTION_03)){
-            List<AccessibilityNodeInfo> changeAccount =  nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/amv");
-            if(changeAccount!=null&&changeAccount.size()>0){
-                AutoUtil.performClick(changeAccount.get(0).getChild(0),loginRecord,Constants.LOGIN_ACTION_04);
-            }
+            AccessibilityNodeInfo changeAccount = AutoUtil.findNodeInfosById(nodeInfo,"com.tencent.mm:id/fl");
+            AutoUtil.performClick(changeAccount,loginRecord,Constants.LOGIN_ACTION_04);
             return;
         }
         //5、使用其他方式登录
         if(AutoUtil.checkAction(loginRecord,Constants.LOGIN_ACTION_04)){
-            AccessibilityNodeInfo changeLoginWayBtn = AutoUtil.findNodeInfosByText(nodeInfo,"使用其他方式登录");
+            AccessibilityNodeInfo changeLoginWayBtn = AutoUtil.findNodeInfosByText(nodeInfo,"用微信号/QQ号/邮箱登录");
             AutoUtil.performClick(changeLoginWayBtn,loginRecord,Constants.LOGIN_ACTION_05);
             return;
         }
         //6、输入账号、密码、点击登录
         if(AutoUtil.checkAction(loginRecord,Constants.LOGIN_ACTION_05)||AutoUtil.checkAction(loginRecord,Constants.LOGIN_ACTION_06)||AutoUtil.checkAction(loginRecord,Constants.LOGIN_ACTION_07)){
-            List<AccessibilityNodeInfo> userNode =  nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/bfm");
+            List<AccessibilityNodeInfo> userNode =  nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/bhg");
             if(userNode!=null&&userNode.size()==1){
                 AutoUtil.performSetText(userNode.get(0).getChild(1),accounts.get(accountIndex)[0],loginRecord,Constants.LOGIN_ACTION_06);
             }
-            List<AccessibilityNodeInfo> pwdNode =  nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/bfn");
+            List<AccessibilityNodeInfo> pwdNode =  nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/bhh");
             if(pwdNode!=null&&pwdNode.size()==1){
                 AutoUtil.performSetText(pwdNode.get(0).getChild(1),accounts.get(accountIndex)[1],loginRecord,Constants.LOGIN_ACTION_07);
             }
             AutoUtil.sleep(500);
             if(AutoUtil.checkAction(loginRecord,Constants.LOGIN_ACTION_07)){
-                AccessibilityNodeInfo loginBtn = AutoUtil.findNodeInfosById(nodeInfo,"com.tencent.mm:id/bfo");
+                AccessibilityNodeInfo loginBtn = AutoUtil.findNodeInfosById(nodeInfo,"com.tencent.mm:id/bhj");
                 AutoUtil.performClick(loginBtn,loginRecord,Constants.LOGIN_ACTION_08);
             }
             if(AutoUtil.checkAction(loginRecord,Constants.LOGIN_ACTION_08)){
@@ -278,6 +349,15 @@ public class ChatService extends AccessibilityService {
                 isLogin=false;
             }
         }
+    }
+    private void showToastByRunnable(final Context context, final CharSequence text) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context,text,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public List<String[]> getAccount(){
@@ -305,5 +385,25 @@ public class ChatService extends AccessibilityService {
             accounts.add(new String[]{u4,p4});
         return accounts;
 
+    }
+    public  static void getChild(AccessibilityNodeInfo node){
+        System.out.println("-----------start---------");
+        if(node!=null){
+            int count = node.getChildCount();
+            System.out.println("child count"+count+"node text-->"+node.getText()+"  node clsName-->"+node.getClassName()+" desc"+node.getContentDescription());
+            if(count>0){
+                for(int i=0,l=count;i<l;i++){
+                    if(i==0){
+                        System.out.println("第"+i+"层 第0个子节点，兄弟节点数:"+count);
+                    }
+                    AccessibilityNodeInfo child = node.getChild(i);
+                    getChild(child);
+                    if(child!=null){
+                        System.out.println(i+" child text-->"+child.getText()+" child clsName-->"+child.getClassName()+" desc"+node.getContentDescription());
+                    }
+                }
+            }
+        }
+        System.out.println("-----------end---------");
     }
 }
